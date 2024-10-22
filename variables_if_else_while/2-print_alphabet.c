d55b477506df968d7e63a5245b4fab2b0fdac902
Doku-Dow <shems.alilat@outlook.fr>
2024-10-22 19:19:44 +0200

    print alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{ char letter
+ for (letter = 'a'; letter <= z; letter++)
+ { letter = tolower(letter);
+	    putchar(letter)
+ }
+}

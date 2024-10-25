c261c61049d7b033e8b6d3d87731615b8e382d42
Doku-Dow <shems.alilat@outlook.fr>
2024-10-25 15:38:33 +0200

    fix for task 4

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,8 +1,10 @@
 #include "main.h"
+
 /**
- *_isalpha - controls if a character is alphabetical
- *@c: character to be verified
- *Return : return 0 or 1
+ * _isalpha - Checks if a character is alphabetic.
+ * @c: The character to be checked.
+ *
+ * Return: 1 if character is letter, lowercase or uppercase, 0 otherwise.
  */
 int _isalpha(int c)
 {

cc7675fa964d642d6188ce7a988e67d3fca79ba9
Doku-Dow <shems.alilat@outlook.fr>
2024-10-25 11:21:58 +0200

    prints the alphabet 10 times

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -10,15 +10,14 @@ void print_alphabet_x10(void)
 	char alphabet;
 
 	while (n < 10)
-	  {
+{
 	alphabet = 'a';
 	while (alphabet <= 'z')
-	  {
+{
 	_putchar(alphabet);
 	alphabet++;
-	  }
+}
 	_putchar('\n');
 	n++;
-	  }
-	return (0);
-  }
+}
+}

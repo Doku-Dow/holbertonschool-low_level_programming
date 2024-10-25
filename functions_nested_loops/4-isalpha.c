680d01b5680e7c74dda39817b71fe7ef66409d62
Doku-Dow <shems.alilat@outlook.fr>
2024-10-25 13:28:46 +0200

    function that checks if a character is alphabetic

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -4,10 +4,11 @@
  *@c: character to be verified
  *Return : return 0 or 1
  */
-int _isalpha (int c)
+int _isalpha(int c)
 {
-if ((c >=65 && c <= 90) || (c >=97 && c <= 122))
-return (1);
-
-return (0);
+	if ((c >= 'a' && c <= 'z') ||
+	    (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
 }

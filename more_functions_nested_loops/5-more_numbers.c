cc5711949450c61d9b7412589c672fd5efd2a476
Doku-Dow <shems.alilat@outlook.fr>
2024-10-29 11:54:26 +0100

    function that prints the numbers from 0 to 14, 10 times, followed by a new line

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *more_numbers - prints numbers from 0 to 14 ten times
+ *
+ *
+ */
+void more_numbers(void)
+{
+	int i = 0;
+	int k;
+
+	while (i < 10)
+{
+	for (k = 0 ; k <= 14 ; k++)
+{
+	if (k > 9)
+{
+	_putchar((k / 10) + '0');
+}
+	_putchar((k % 10) + '0');
+}
+	_putchar('\n');
+	i++;
+}
+}

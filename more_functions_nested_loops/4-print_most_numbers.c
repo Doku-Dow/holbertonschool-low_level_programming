5f43d16059ecdcbd6513854200926a154bb1b2ef
Doku-Dow <shems.alilat@outlook.fr>
2024-10-29 11:50:02 +0100

    function that prints numbers from 0 to 9 except 2 and 4

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- *function that prints the numbers from 0 to 9 but not 2 and 4
+ *print_most_numbers - prints the numbers from 0 to 9 but not 2 and 4
  *
  */
 void print_most_numbers(void)

a9d3133cee77ef0f763d940039263cbf7c8cebf5
Doku-Dow <shems.alilat@outlook.fr>
2024-11-07 13:43:48 +0100

    program that prints its name, followed by a new line

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+*main - prints the program name, followed by a new line.
+*@argc - the number of arguments given to the program
+*@argv - the array of pointers to the arguments
+*/
+
+int main (int argc, char *argv[])
+{
+(void)argc;
+  
+printf("%s\n", argv[0]);
+return 0;
+}

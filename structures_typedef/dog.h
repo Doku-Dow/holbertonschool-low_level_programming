8bb80ee5daba5c71a2697a958b17a89055d1744b
Doku-Dow <shems.alilat@outlook.fr>
2024-11-14 10:25:36 +0100

    update

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,8 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
 
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
 *struct dog - a new type of data
@@ -29,7 +27,5 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-int _strlen(char *str);
-char *_strcopy(char *dest, char *src);
 
 #endif

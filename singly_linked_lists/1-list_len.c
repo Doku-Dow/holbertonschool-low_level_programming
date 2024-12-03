73b5e71e965cc57bae8d8fe676d19d7801792dc3
Doku-Dow <shems.alilat@outlook.fr>
2024-12-03 19:25:48 +0100

    fixed 1-list_len.c

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,33 +1,20 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "lists.h"
 
 /**
- * main - check the code
- * Return: Always 0.
- */
-int main(void)
+*list_len - Finds the number of elements in
+*            a linked list_t list.
+*@h: The linked list_t list.
+*Return: The number of elements in h.
+*/
+size_t list_len(const list_t *h)
 {
-list_t *head;
-list_t *new;
-list_t hello = {"World", 5, NULL};
-size_t n;
+size_t elements = 0;
 
-head = &hello;
-new = malloc(sizeof(list_t));
-if (new == NULL)
+while (h)
 {
-printf("Error\n");
-return (1);
+elements++;
+h = h->next;
 }
-new->str = strdup("Hello");
-new->len = 5;
-new->next = head;
-head = new;
-n = list_len(head);
-printf("-> %lu elements\n", n);
-free(new->str);
-free(new);
-return (0);
+
+return (elements);
 }
